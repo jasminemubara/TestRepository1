342e67aa9b97d8c23e4de8fc4d373c9cd7ea822c
jasminemubara <52207724+jasminemubara@users.noreply.github.com>
2024-04-29 10:38:07 +0500

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class Assignment3 {
+    public static void main(String arg[]) {
+
+        int num = 45;
+        if (num%2 == 0){
+            System.out.println("QA jobs in Lahore");
+        }
+        else{
+            System.out.println("Mobile automation testing course");
+        }
+    }
+}
+
+//Question 03
+        //        int table = 9;
+
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println(table + " x " + i + " = " + (table * i));
+        //}
+
+
+
+
+
+//Question 02
+//         String name = "Mubarra"
+//         int age = 26;
+//         double pi = 3.14;
+//         boolean isTrue = true;
+//         char grade = 'A';
+//         byte smallNum = 5;
+
+//Question 01
+//        String name = "Mubarra";
+//        int age = 26;
+//        float marks = 45.6F;
+//
+//        System.out.println("Name:" + name);
+//        System.out.println("Name:" + age);
+//        System.out.println("Name:" + marks);
